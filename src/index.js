1e4246d985ebbc29f0cc87a1bddb17f571ba6898
YDLucero <yohanna.lucero@gmail.com>
2024-04-26 14:40:24 -0300

    react hooks

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './assets/css/app.css';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
